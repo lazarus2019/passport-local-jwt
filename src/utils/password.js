b6fd4d1efccfdd968074b1e8995a8495788565dc
NTSon <thaisonbk2020@gmail.com>
2022-10-13 22:16:36 +0700

    Add bcryptjs hash methods

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,4 +1,5 @@
 const CryptoJS = require("crypto-js");
+const bcrypt = require("bcryptjs");
 
 const encodePassword = (password) => {
   return CryptoJS.AES.encrypt(password, process.env.PASSWORD_SECRET_KEY);
@@ -9,6 +10,17 @@ const decodePassword = (password) => {
     password,
     process.env.PASSWORD_SECRET_KEY
   ).toString(CryptoJS.enc.Utf8);
+};
+
+// BcryptJS
+
+const BCryptEncode = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hashSync(password, salt);
+};
+
+const BCryptDecode = (enterPass, userPass) => {
+  return bcrypt.compare(enterPass, userPass);
 };
 
 module.exports = {
